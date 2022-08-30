a9810ce050425ca3cd33383e52aa9149344c57af
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2022-08-30 14:18:45 +0200

    fix: delete code that is not needed in this module

--- main.tf
+++ main.tf
@@ -20,12 +20,6 @@ resource "argocd_project" "this" {
       namespace = var.namespace
     }
 
-    # TODO verify if this should be here
-    destination {
-      name      = "in-cluster"
-      namespace = "kube-system"
-    }
-
     orphaned_resources {
       warn = true
     }
