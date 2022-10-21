5eb75ed8b1b861fc5291127e24c695529ebc98be
Jérémy Barascut <jeremy.barascut@camptocamp.com>
2022-10-21 15:50:07 +0200

    fix(aks): add azure main

--- aks/main.tf
+++ aks/main.tf
@@ -12,11 +12,16 @@ module "kube-prometheus-stack" {
   source = "../"
 
   cluster_name     = var.cluster_name
-  oidc             = var.oidc
   argocd_namespace = var.argocd_namespace
   base_domain      = var.base_domain
   cluster_issuer   = var.cluster_issuer
+  namespace        = var.namespace
+  dependency_ids   = var.dependency_ids
+
   metrics_archives = var.metrics_archives
+  prometheus       = var.prometheus
+  alertmanager     = var.alertmanager
+  grafana          = var.grafana
 
   helm_values = concat(local.helm_values, var.helm_values)
 }
