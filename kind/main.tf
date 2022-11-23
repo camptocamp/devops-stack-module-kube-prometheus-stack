c105706d449e8d7d5ff6eb6a40f10a5ea6c6889b
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2022-11-23 13:35:16 +0100

    feat: add kind variant

---
+++ kind/main.tf
@@ -0,0 +1,18 @@
+module "kube-prometheus-stack" {
+  source = "../"
+
+  cluster_name     = var.cluster_name
+  argocd_namespace = var.argocd_namespace
+  base_domain      = var.base_domain
+  cluster_issuer   = var.cluster_issuer
+  namespace        = var.namespace
+  dependency_ids   = var.dependency_ids
+
+  prometheus   = var.prometheus
+  alertmanager = var.alertmanager
+  grafana      = var.grafana
+
+  metrics_storage_main = local.metrics_storage_main
+
+  helm_values = concat(local.helm_values, var.helm_values)
+}
