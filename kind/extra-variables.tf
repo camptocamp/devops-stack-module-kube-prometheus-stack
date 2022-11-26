c4bca1ce8c9169cc2b96045e98a1a5a1ba5e288a
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2022-11-26 13:19:57 +0100

    fix: remove enabled flag in the kind variant

--- kind/extra-variables.tf
+++ kind/extra-variables.tf
@@ -1,7 +1,6 @@
 variable "metrics_storage" {
   description = "MinIO S3 bucket configuration values for the bucket where the archived metrics will be stored."
   type = object({
-    enabled           = bool
     bucket_name       = string
     endpoint          = string
     access_key        = string
@@ -11,7 +10,6 @@ variable "metrics_storage" {
   # of `null` because we Terraform does not like when we try to insert null values inside of strings, which we do when
   # parsing some of these values inside of a string to create a new one.
   default = {
-    enabled           = false
     bucket_name       = ""
     endpoint          = ""
     access_key        = ""
