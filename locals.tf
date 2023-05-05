a3c7428e16eb9c0513e70f17de5b73aa12ea48e6
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2023-05-05 12:44:21 +0200

    feat: enable Grafana by default and ignore self-signed certificates

--- locals.tf
+++ locals.tf
@@ -80,14 +80,15 @@ locals {
         adminPassword = "${replace(local.grafana.admin_password, "\"", "\\\"")}"
         "grafana.ini" = {
           "auth.generic_oauth" = merge({
-            enabled       = true
-            allow_sign_up = true
-            client_id     = "${replace(local.grafana.oidc.client_id, "\"", "\\\"")}"
-            client_secret = "${replace(local.grafana.oidc.client_secret, "\"", "\\\"")}"
-            scopes        = "openid profile email"
-            auth_url      = "${replace(local.grafana.oidc.oauth_url, "\"", "\\\"")}"
-            token_url     = "${replace(local.grafana.oidc.token_url, "\"", "\\\"")}"
-            api_url       = "${replace(local.grafana.oidc.api_url, "\"", "\\\"")}"
+            enabled                  = true
+            allow_sign_up            = true
+            client_id                = "${replace(local.grafana.oidc.client_id, "\"", "\\\"")}"
+            client_secret            = "${replace(local.grafana.oidc.client_secret, "\"", "\\\"")}"
+            scopes                   = "openid profile email"
+            auth_url                 = "${replace(local.grafana.oidc.oauth_url, "\"", "\\\"")}"
+            token_url                = "${replace(local.grafana.oidc.token_url, "\"", "\\\"")}"
+            api_url                  = "${replace(local.grafana.oidc.api_url, "\"", "\\\"")}"
+            tls_skip_verify_insecure = var.cluster_issuer == "ca-issuer" ? true : false
           }, local.grafana.generic_oauth_extra_args)
           users = {
             auto_assign_org_role = "Editor"
@@ -326,7 +327,7 @@ locals {
   }]
 
   grafana_defaults = {
-    enabled                  = false
+    enabled                  = true
     additional_data_sources  = false
     generic_oauth_extra_args = {}
     domain                   = "grafana.apps.${var.cluster_name}.${var.base_domain}"
