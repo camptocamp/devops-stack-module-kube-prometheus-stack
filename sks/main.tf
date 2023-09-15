d2ed077780547e01294f6fca03b7c012f304cb06
Gonçalo Heleno <goncalo.heleno@camptocamp.com>
2023-09-15 14:20:22 +0200

    feat: add standard variables and variable to add labels to Argo CD app

--- sks/main.tf
+++ sks/main.tf
@@ -5,6 +5,7 @@ module "kube-prometheus-stack" {
   base_domain            = var.base_domain
   argocd_namespace       = var.argocd_namespace
   argocd_project         = var.argocd_project
+  argocd_labels          = var.argocd_labels
   destination_cluster    = var.destination_cluster
   target_revision        = var.target_revision
   cluster_issuer         = var.cluster_issuer
